028b97faa35be535f784f8e810bb855652ac447a
Caitlin <leftielulu1995@bell.net>
2020-12-06 16:13:41 -0500

    created app layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
-import axios from "axios";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 
 /* PSEUDOCODE
@@ -33,11 +34,44 @@ import './App.css';
       - endpoint?:  http://api.tvmaze.com/schedule?country=:countrycode&date=:date
         - country: CA
           - param in axios call (hardcoded)
+          - US has more data; use this instead? HelpCue
         - date: YEAR-MO-DA (variable)
       - make the API call based on the above parameters
       - save that response to our state array object
     - map through array and render TV shows on the screen and apply filter to narrow down by genre
     - allow the user to start over via button that will send them back to VIEW ONE
+
+    - REVIEW does time zone matter?
+    - retrieve from API:
+      - airdate --> YEAR-MO-DA
+      - airstamp?
+        - NOTE airstamp is UTC
+      - airtime --> 24hr clock
+        - NOTE airtime is AST
+      - id
+      - image
+      - show
+        - genres
+          - NOTE check if the array is empty
+        - image
+          - NOTE check if the object is empty
+          - check if first image property is empty/null
+        - language --> English only
+        - name
+        - official site --> STRETCH
+        - rating? --> some are null
+        - runtime --> STRETCH
+        - schedule
+          - days (array)
+          - time --> if airtime is null? error handling?
+        - status
+          - NOTE check for "Running" status
+        - summary --> STRETCH
+
+        - NOTE find alt text!
+          - use name (for now)
+            - show name + episode name/number
+
 
     Good job Team! :)
 */
@@ -54,7 +88,8 @@ class App extends Component {
     }
   }
 
-  //AXIO call for movie list
+  // NOTE MOVE TO VIEW THREE
+  //AXIOS call for movie list
   componentDidMount() {
     axios({
       method: "GET",
@@ -76,12 +111,6 @@ class App extends Component {
   }
 
 
-
-
-
-
-
-
   // Reload page button if AXIOS error
   reloadPage = () => {
     window.location.reload(false);
@@ -90,16 +119,33 @@ class App extends Component {
   // Display data
   render() {
     return (
-      <div className="App">
-        {/* Show error message if AXIOS didn't work, button to reload the page */}
-        {this.state.showErrorMessage && <div className="blockView">
-          <div className="error">
-            <h6>Sorry... Something went wrong, not all data can be retrieved.</h6>
-            <button onClick={this.reloadPage}>Try again!</button>
-            <p>{this.state.errorMessage}</p>
-          </div>
-        </div>}
-      </div>
+      <Router>
+        <div className="App">
+
+        {/* STRETCH: Homepage */}
+
+        {/* COMPONENT ONE: SocialEvents */}
+        {/* contains social event cards, title, INSTRUCTIONS */}
+
+        {/* COMPONENT TWO: Search; Schedule; ScheduleSelection; ScheduleFilter; Filter */}
+        {/* contains: Calendar and SelectGenre components */}
+          {/* submit button */}
+
+        {/* COMPONENT THREE: ResultsPage */}
+        {/* contains: social event component, filtered tv shows component (AXIOS), pick again button */}
+
+          {/* Show error message if AXIOS didn't work, button to reload the page */}
+          {this.state.showErrorMessage && <div className="blockView">
+            <div className="error">
+              <h6>Sorry... Something went wrong, not all data can be retrieved.</h6>
+              <button onClick={this.reloadPage}>Try again!</button>
+              <p>{this.state.errorMessage}</p>
+            </div>
+          </div>}
+
+
+        </div>
+      </Router>
     );
   }
 }
