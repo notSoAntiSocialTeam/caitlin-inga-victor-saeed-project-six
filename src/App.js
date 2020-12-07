5b8da4aab7a540ca42165f25ac86c47a4ea1326a
saeedmalak <makhh2001@gmail.com>
2020-12-07 13:02:44 -0500

    added SASS to our app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import axios from 'axios';
-import './App.css';
+import './sass/App.scss';
 
 /* PSEUDOCODE
   - Firebase --> VIEW ONE
